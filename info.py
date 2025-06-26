066197eda6a57d6c839bc4fc9e28fefed8a3e023
itskrishname <kd259457@gmail.com>
2025-06-26 14:11:42 +0530

    Update info.py

--- info.py
+++ info.py
@@ -5,14 +5,14 @@ from os import environ
 SESSION = environ.get('SESSION', 'TechVJBot')
 
 # Your Telegram Account Api Id And Api Hash
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_ID = int(environ.get('API_ID', '24828197'))
+API_HASH = environ.get('API_HASH', 'd36e278e89ebeb900aeda4128d413a77')
 
 # Bot Token, This Is Main Bot
 BOT_TOKEN = environ.get('BOT_TOKEN', "")
 
 # Admin Telegram Account Id For Withdraw Notification Or Anything Else
-ADMIN = int(environ.get('ADMIN', ''))
+ADMIN = int(environ.get('ADMIN', '7660990923'))
 
 # Back Up Bot Token For Fetching Message When Floodwait Comes
 BACKUP_BOT_TOKEN = environ.get('BACKUP_BOT_TOKEN', "")
